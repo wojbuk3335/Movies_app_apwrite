5352c2623cb44547ebcda6d26ba4590ed5028c24
Wojtek <w.bukowski1985@gmail.com>
2025-05-02 06:39:40 +0200

    Inicial commit 2

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,11 @@ import { Stack } from "expo-router";
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return(  
+  <Stack screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <Stack.Screen name="movies" options={{ headerShown: false }} />
+  </Stack>
+  );
+
 }
